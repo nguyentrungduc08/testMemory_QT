53b51676cabb268754770dc45d29e52d145b5a1b
hydra <nguyentrungduc08@gmail.com>
2018-04-27 14:25:02 +0700

    init code

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QList>
+#include <QVector>
+#include <iostream>
+
+
+class uQlist {
+private:
+    QList list;
+public:
+
+
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
---
+++ main.qml
@@ -0,0 +1,13 @@
+import QtQuick 2.9
+import QtQuick.Window 2.2
+import QtQuick.Controls 1.4
+
+Window {
+    visible: true
+    width: 640
+    height: 480
+    title: qsTr("Hello World")
+
+
+
+}
