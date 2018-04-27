767cd5ef45fd907ebc43dd910cb5216b47c854ed
hydra <nguyentrungduc08@gmail.com>
2018-04-27 15:56:11 +0700

    update object test

--- main.cpp
+++ main.cpp
@@ -4,14 +4,57 @@
 #include <QVector>
 #include <iostream>
 
-
-class uQlist {
+class objectTest{
 private:
-    QList list;
+    QString             _proString;
+    int                 _proInt;
+
 public:
+    objectTest(QString proString, int proInt) : _proString(proString), _proInt(proInt){
+        std::cout << "created object" << std::endl;
+    };
+
+    ~objectTest(){
+        std::cout << "released object" << std::endl;
+    };
+
+};
 
 
-}
+class uQList {
+private:
+    QList<objectTest>       _list;
+
+public:
+    uQList(){
+        std::cout << "created object uQList" << std::endl;
+    };
+
+    ~uQList(){
+        std::cout << "released object uQList" << std::endl;
+    };
+
+    void setData(){
+        for (int i = 0; i < 1000; ++i)
+        {
+            objectTest test("testdata",0);
+            this->_list.append(test);
+        }
+    };
+
+};
+
+class uQVector {
+private:
+    QVector<objectTest>     _vector;
+
+public:
+    uQVector(){
+
+    }
+
+};
+
 
 int main(int argc, char *argv[])
 {
@@ -23,6 +66,10 @@ int main(int argc, char *argv[])
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
+
+    {
+        uQList qlist;
+    }
 
     return app.exec();
 }
